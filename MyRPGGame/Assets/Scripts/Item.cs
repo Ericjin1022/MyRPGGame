9e9b03afe11a0c649baa60130c92fa2a106ef91a
ericjin1022 <1105270750@qq.com>
2021-04-20 17:41:22 +0800

    EP04..待续

---
+++ MyRPGGame/Assets/Scripts/Item.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Item : ScriptableObject
+{
+    new public string name = "New Item";
+    public Sprite icon = null;
+    public bool isDefaultItem = false;
+    //5:15
+    
+}
